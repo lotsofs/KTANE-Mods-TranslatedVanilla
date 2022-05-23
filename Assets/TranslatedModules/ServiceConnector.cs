266247173062e93beeb39d05b77c4ddbb3f3aca1
lotsofs <oeneboy@hotmail.com>
2022-05-23 16:52:47 +0100

    Something about a service connector?

---
+++ Assets/TranslatedModules/ServiceConnector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceConnector : MonoBehaviour {
+	private static GameObject _serviceObject;
+
+	public void GetService() {
+		_serviceObject = GameObject.Find("TranslatedModulesService(Clone)");
+		GameObject stickerObject = null;
+		if (_serviceObject == null) {
+			// translated modules service not installed
+			return null;
+		}
+	}
+
+	public static GameObject GetSticker() {
+
+	}
+}
